0ea6d921d915ae8c1cf9ff7d70ba9dfb6df53b40
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-04-13 21:33:10 +0700

    Create code.kt

---
+++ Kotlin Bootcamp for Programmers/Kotlin Basics/Quiz Strings/code.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var welcomeMessage = "Hello and welcome to Kotlin"
+	when (welcomeMessage.length) {
+        0 -> println("Nothing to say?")
+        in 1..50 -> println("Perfect")
+        else -> println("Too long!")
+    }
+}
